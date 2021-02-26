d954fc820eee558156ce03b9c0105c64fef04e56
Michal-Kok <pukkix17@gmail.com>
2021-02-26 11:06:03 +0100

    return button added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,31 @@
 import '../styles/nav.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import React, { useState, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import NavigationOverlay from '../components/NavigationOverlay';
 import { ReactComponent as Logo } from '../img/Logo.svg';
 
 function Nav() {
     const [isActive, setIsActive] = useState(false);
     const burgerRef = useRef();
+    const history = useHistory();
 
     const handleBurgerClick = () => {
         burgerRef.current.classList.toggle('activeBurger');
         setIsActive(!isActive);
     }
 
+    const handleGoBackClick = () => history.goBack();
+
     return (
         <>
             <div className="navContainer">
+                <button
+                    className="navContainer__goBackButton button"
+                    onClick={handleGoBackClick} >
+                    <FontAwesomeIcon icon={faAngleLeft} />
+                </button>
                 <div className="navContainer__logo">< Logo /></div>
                 <div className="navContainer__burgerWrapper">
                     <button
