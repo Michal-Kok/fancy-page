1a5064b9f176acefd75c90019e8be5845eced0be
Michal-Kok <pukkix17@gmail.com>
2021-03-08 16:54:33 +0100

    part of about page finished

--- src/components/OfferPlaces.jsx
+++ src/components/OfferPlaces.jsx
@@ -1,12 +1,62 @@
 import '../styles/offerPlaces.scss';
-import React, { } from 'react';
+import React, { useEffect } from 'react';
 import { ReactComponent as EuropeSVG } from '../img/europe.svg';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
 function OfferPlaces() {
 
     const title = "We have travelled and helped people in so many places in Europe";
-    const textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    const textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.";
+
+    useEffect(() => {
+        const title = document.querySelector('.offerPlaces__mapContainer__title');
+        const pins = document.querySelectorAll('.pin');
+        const container = document.querySelector('.offerPlaces__mapContainer');
+        const europe = document.querySelector('#europeSVG');
+        gsap.set(title, { opacity: 0, xPercent: -100 });
+        gsap.set(pins, { opacity: 0, scale: 0 });
+        gsap.set(europe, { opacity: 0, yPercent: 200 });
+
+
+        gsap.registerPlugin(ScrollTrigger);
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: container,
+                start: 'top 30%',
+            }
+        });
+
+        tl.to(
+            title,
+            {
+                opacity: 1,
+                xPercent: 0,
+                duration: .6,
+                ease: "expo.out",
+            }
+        ).to(
+            europe,
+            {
+                opacity: 1,
+                yPercent: 0,
+                duration: .6,
+                ease: "expo.out",
+            }
+        ).to(
+            pins,
+            {
+
+                opacity: 1,
+                scale: 1,
+                duration: .4,
+                stagger: .4,
+                ease: "expo.out",
+            }
+        );
+    })
+
     return (
         <section className="offerPlaces">
             <div className="offerPlaces__mapContainer">
