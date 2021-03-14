d73971a149983033373a428775e17f6e35bdbfc5
Michal-Kok <pukkix17@gmail.com>
2021-03-14 21:10:40 +0100

    Animations are added to opinions section

--- src/components/Opinions.jsx
+++ src/components/Opinions.jsx
@@ -1,7 +1,41 @@
 import '../styles/opinions.scss';
 import OpinionsList from '../components/OpinionsList';
+import { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function Opinions() {
+
+    useEffect(() => {
+        const opinionsSection = document.querySelector('.opinions');
+        const OpinionsList = document.querySelector('.opinions__list');
+
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.set(opinionsSection.children, { opacity: 0, y: 150 });
+        gsap.set(OpinionsList, { opacity: 1, y: 0 });
+
+        const sectionTimeline = gsap.timeline({
+            delay: .4,
+            scrollTrigger: {
+                trigger: opinionsSection,
+                start: 'top 70%',
+                toggleActions: "play pause resume reset",
+                // markers: true,
+            }
+        });
+
+        sectionTimeline.to(
+            opinionsSection.children,
+            {
+                opacity: 1,
+                y: 0,
+                duration: .5,
+                stagger: .4,
+                ease: "ease-in-out",
+            }
+        );
+
+    }, []);
 
     return (
         <section className="opinions">
--- src/components/OpinionsList.jsx
+++ src/components/OpinionsList.jsx
@@ -2,6 +2,8 @@ import '../styles/opinions.scss'
 import { clientData } from '../api';
 import OpinionElement from '../components/OpinionElement';
 import { useEffect, useState } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function OpinionsList() {
 
@@ -13,6 +15,36 @@ function OpinionsList() {
             .then(({ data }) => setData(data.results))
             .catch(err => console.error(err));
     }, []);
+
+    // second useEffect created for animation
+
+    useEffect(() => {
+        const OpinionsList = document.querySelector('.opinions__list');
+
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.set(OpinionsList.children, { opacity: 0, xPercent: -100 });
+
+        const listTimeline = gsap.timeline({
+            delay: .8,
+            scrollTrigger: {
+                trigger: OpinionsList,
+                start: 'top 90%',
+                // markers: true,
+                toggleActions: "play pause resume reset",
+            }
+        });
+
+        listTimeline.to(
+            OpinionsList.children,
+            {
+                opacity: 1,
+                xPercent: 0,
+                duration: .5,
+                stagger: .2,
+                ease: "ease-in-out",
+            }
+        )
+    })
 
     return (
         <div className="opinions__list">
