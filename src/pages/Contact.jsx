2502c7972c8b1834d1c7be0ac84f1b83c45c1afd
Michal-Kok <pukkix17@gmail.com>
2021-03-16 22:27:11 +0100

    form closing problem fixed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,14 +3,14 @@ import Header from '../components/Header';
 import Form from '../components/Form';
 import React, { useState, useRef } from 'react';
 import { gsap } from "gsap";
-import { ReactComponent as CircleSVG } from '../img/contactCircle.svg';
 
 function Contact() {
     const [isActive, setIsActive] = useState(false);
     const overlayRef = useRef();
 
     const title = "Feel free to register";
-    const buttonContent = "Sign in";
+    const buttonOpen = "Sign in";
+    const buttonClose = "Close!";
 
     const headerHandleClick = () => {
         const overlay = overlayRef.current;
@@ -22,15 +22,28 @@ function Contact() {
             leftValue = "50%";
             widthValue = "50vw"
         }
-        tl.to(
-            overlay,
-            {
-                left: leftValue,
-                borderRadius: 0,
-                duration: .4,
-                width: widthValue,
-            }
-        );
+
+        if (!isActive) {
+            tl.to(
+                overlay,
+                {
+                    left: leftValue,
+                    borderRadius: 0,
+                    duration: .4,
+                    width: widthValue,
+                }
+            );
+        } else {
+            tl.to(
+                overlay,
+                {
+                    left: '90%',
+                    borderRadius: '50%',
+                    duration: .4,
+                    width: '100vh',
+                }
+            );
+        }
         setIsActive(!isActive);
     };
 
@@ -38,7 +51,7 @@ function Contact() {
         <section className="contactPage">
             < Header
                 title={title}
-                buttonContent={buttonContent}
+                buttonContent={isActive ? buttonClose : buttonOpen}
                 headerHandleClick={headerHandleClick} />
             <div
                 className="contactPage__overlayContainer"
